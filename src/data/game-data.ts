e3f46b8b92b6ab01c45c64172e9782f9345f3f61
Till Hinrichs <till@eightdaysaweek.cc>
2023-07-14 07:30:35 +0200

    feat: added ordinals pokemon

--- src/data/game-data.ts
+++ src/data/game-data.ts
@@ -1,3 +1,4 @@
+import ordinalsPokemonImage from "../assets/ordinals-pokemon-game-cover.webp";
 import ordjakImage from "../assets/ordjak-game-cover.webp";
 import wizordsImage from "../assets/wizords-game-cover.webp";
 
@@ -17,6 +18,11 @@ const gameData: GameData = [
     title: "Ordjak",
     image: ordjakImage,
     id: "005cac3862fe816b8217b3cff44e72b14a5e040d4f00302a468cd43d1a86ee2ei0",
+  },
+  {
+    title: "Ordinals Pokemon",
+    image: ordinalsPokemonImage,
+    id: "695eaf226e0bb64037bc9e3f11042da764ad0c5a4b9d1664a40925942c108f84i0",
   },
 ];
 
