4bd833c21e083bd478bbbec72f9bac57b447ed30
Till Hinrichs <till@eightdaysaweek.cc>
2023-07-15 10:08:21 +0200

    fix: wrong player card indicators

--- src/components/cards/player-card.tsx
+++ src/components/cards/player-card.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { twMerge } from "tailwind-merge";
 import { capitalizeFirstLetter } from "../../utils/string";
-import SupplyIndicator from "../atoms/minted-indicator";
+import MintedIndicator from "../atoms/minted-indicator";
+import SupplyIndicator from "../atoms/supply-indicator";
 
 export interface PlayerCardProps {
   playerClass: string;
@@ -10,10 +9,6 @@ export interface PlayerCardProps {
 }
 
 const PlayerCard = ({ playerClass, supply, totalSupply }: PlayerCardProps) => {
-  const percentageMinted = React.useMemo(() => {
-    return totalSupply ? (totalSupply - supply) / totalSupply : undefined;
-  }, [supply, totalSupply]);
-
   return (
     <div className="flex flex-col p-4 space-y-2 bg-white rounded-lg dark:bg-secondary-900">
       <h3 className="flex-grow text-base font-medium text-secondary-900 dark:text-secondary-100">
@@ -29,19 +24,7 @@ const PlayerCard = ({ playerClass, supply, totalSupply }: PlayerCardProps) => {
         <span className="flex-grow block font-medium leading-none tracking-widest uppercase truncate text-2xs text-secondary-500">
           Minted
         </span>
-        {percentageMinted && (
-          <span
-            className={twMerge(
-              "block font-medium leading-none text-right text-xs text-secondary-500",
-              percentageMinted === 0 && "text-red-500",
-              percentageMinted <= 0.1 && "text-orange-500",
-              percentageMinted < 0.5 && "text-yellow-500",
-              percentageMinted >= 0.5 && "text-green-500"
-            )}
-          >
-            {Math.ceil(percentageMinted * 100)} %
-          </span>
-        )}
+        <MintedIndicator supply={supply} totalSupply={totalSupply} />
       </p>
     </div>
   );
